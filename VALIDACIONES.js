b7bb05d65c51c9ea89985cd6c079e1072b335868
Jose Mendez <josemendezlbn@gmail.com>
2020-07-24 18:49:29 -0400

    Actualice el archivo validaciones.js y agregue cambios pendientes

--- VALIDACIONES.js
+++ VALIDACIONES.js
@@ -3,41 +3,25 @@ VALIDACIONES
 MODULO CICLO DE CONSECHA
 
 MODULO PRODUCTOR
-* Validar que el usuario de tipo productor no pueda registrar un nuevo productor sino solo modificar algunos de sus datos
-* Validar que los usuarios de tipo entidad puedan crear nuevos registros
-//El buscador de productores registrados debe estar filtrado por entidad
-//obtener todos los datos del productor en la tabla buscar
-//colocar y programar boton de editar productores
-//activar variable updateproductor en metodo edit()
-//bloquear campos de texto necesarios en metodo edit()
-//Colocar y programas boton de limpiar
-
-FORMULARIO PARA REGISTRAR NUEVOS USUARIOS
+* Permitir que una entidad pueda vincular un productor ya existente
 
 MODULO UNIDAD DE PRODUCCION
-//* arreglar sql para buscar las unidades de produccion
-//* solucionar problema con variable update, no se esta mandando por post
 
 MODULO ENTES FINANCIEROS
 
 CLASES Y METODOS PRINCIPALES
-* Modificar metodo select(sql) para que capture uno o mas parametros de busqueda
-* Modificar metodo select(sql) para que la variable campos acepte arreglo o string
-* Crear metodo para validar y configurar niveles de usuario
-
-MODULO DE REGISTRO DE USUARIO
-//*Al registrar un nuevo usuario, si hay un productor registrado o no, el usuario debera también registrar su unidad de produccion. Este es el primer paso en la plataforma
-//* El segundo paso es registrar una intension de siembra
+* Crear metodo en PHP para validacion de formulario. Al igual que el metodo JavaScript
+* Establecer limites para los tipos de usuario
+	** Los usuario de tipo municipal no podran registrar nada, solo ver informacion.
 
 CICLOS
-* El sistema solo mostrara 3 ciclos: invierno, verano y ciclo largo
-* El ciclo largo no tendra fecha de cierre automatico, sino manual. El administrador es quien determina cuando cierra el ciclo y cuando abre uno nuevo
-************ el estatus de los cilos pasados será 0, el de los ciclos presentes 2 y el del ciclo actual sera 1
+* Crear metodo para cerrar un ciclo al pasar la fecha limite
 
 INTENCION SIEMBRA
-//* las hectareas de la intencion de siembra no pueden ser mayores a las hectareas productivas
-//* Ajustar monto en ha_intencion_total en tabla de intencion despues de eliminar intenciones
-//* validar que las hectareas de la intencion concuerden con las disponibles
-//* modificar consultas en guardado de intencion. Que guarde nuevo y actualize **************************************************************
+* Verificar si la nueva intencion coincide con una intencion ya registrada con el mismo ciclo, entidad, productor, unidad de produccion y rubro. En cuyo caso no debera permitir registrar la nueva intencion.
+* Modificar funcionabilidad y diseño del modulo. Al seleccionar el productor y la unidad de produccion debera proveer informacion adicional como la razon social, nombre, las hectareas disponibles, etc.
+* Crear reportes
 
 DISEÑO Y MODULOS GENERALES
+* Mejorar diseño y funcionabilidad del menu.
+* Modificar diseno de inputsearch. Agregar x para borrar lo escrito
